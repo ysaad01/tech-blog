01f7c6ff2e75b3a6d17de339842ea5a06c55d66d
ysaad01 <ysaad2325@gmail.com>
2022-05-15 16:43:33 -0400

    updated login and logout features

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -5,9 +5,7 @@ async function loginFormHandler(event) {
   const email = document.querySelector("#email-login").value.trim();
   const password = document.querySelector("#password-login").value.trim();
 
-  // if both fields have content
   if (email && password) {
-    // POST to the login route with the user information
     const response = await fetch("/api/users/login", {
       method: "post",
       body: JSON.stringify({
@@ -16,7 +14,6 @@ async function loginFormHandler(event) {
       }),
       headers: { "Content-Type": "application/json" },
     });
-    // when the fetch promise is fufilled, check the response status; if the response is good, load the dashboard; if there is an error, alert with the status
     if (response.ok) {
       document.location.replace("/dashboard");
     } else {
